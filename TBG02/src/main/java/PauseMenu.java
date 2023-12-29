79d6c743518783d9f6adf2ee741cbf92b8301c72
nevesvasco <a043335@umaia.pt>
2023-12-29 22:48:30 +0000

    Menu de pausa

--- TBG02/src/main/java/PauseMenu.java
+++ TBG02/src/main/java/PauseMenu.java
@@ -20,22 +20,19 @@ public class PauseMenu {
     public void draw(TextGraphics graphics, int width , int height ) throws IOException {
         graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
         graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width * 16, height * 9), ' ');
-        // Desenha a caixa do menu de pausa
+
         graphics.setForegroundColor(TextColor.ANSI.BLACK);
         graphics.setBackgroundColor(TextColor.ANSI.WHITE);
 
         graphics.fillRectangle(new TerminalPosition(width - 45, height / 4 - 5), new TerminalSize(40, 10), ' ');
 
-        // Desenha o texto "Pause"
         graphics.putString(new TerminalPosition(width - 28, height / 4 - 4), "Pause");
 
-        // Desenha os botões
         graphics.putString(new TerminalPosition(width - 28, height / 4 - 1), "[Continue]");
 
-        // Desenha os botões
         graphics.putString(new TerminalPosition(width - 28, height / 4 + 1), "[Restart]");
 
-        //Desenha a caixa de seleção de mudo e rótulo
         graphics.putString(new TerminalPosition(width - 45, height / 4 - 5), "[ ] Mute");
+
     }
 }
