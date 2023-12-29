b6dfa4b8ce95082c1e7a04edc65b66ce61597c0b
DanielNunesPT <131387962+DanielNunesPT@users.noreply.github.com>
2023-12-29 20:33:40 +0000

    pause menu

---
+++ TBG02/src/main/java/PauseMenu.java
@@ -0,0 +1,62 @@
+import com.googlecode.lanterna.*;
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.input.KeyType;
+import com.googlecode.lanterna.screen.Screen;
+
+import java.io.IOException;
+
+public class PauseMenu {
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    private Position position;
+
+    public PauseMenu(int x, int y) {
+        position.setX(x);
+        position.setY(y);
+    }
+
+    public void draw(Arena arena,Screen screen,int width , int height ) throws IOException {
+        screen.clear();
+        TextGraphics graphics = screen.newTextGraphics();
+        graphics.setBackgroundColor(TextColor.Factory.fromString("#336699"));
+        graphics.fillRectangle(new TerminalPosition(0, 0), new TerminalSize(width, height), ' ');
+        graphics.setForegroundColor(TextColor.ANSI.WHITE);
+        graphics.putString(new TerminalPosition(width / 2 - 5, height / 2), "Game Paused");
+
+
+        graphics.setForegroundColor(TextColor.ANSI.GREEN);
+        graphics.putString(new TerminalPosition(width / 2 - 5, height / 2 + 2), "Unpause (space)");
+
+
+        graphics.setForegroundColor(TextColor.ANSI.RED);
+        graphics.putString(new TerminalPosition(width / 2 - 5, height / 2 + 4), "Quit (q)");
+
+
+        com.googlecode.lanterna.input.KeyStroke key = screen.pollInput();
+
+
+        while (key != null) {
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == ' ') {
+
+                arena.setRunning(true);
+                screen.refresh();
+                break;
+            }
+
+            if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+
+                screen.close();
+                System.exit(0);
+            }
+
+
+            key = screen.pollInput();
+        }
+    }
+}
